4fa5d64626313ab1f6b788b1b63ee11845c6254a
Alexey Donov <alexeydonov@me.com>
2016-11-08 17:15:56 +0300

    Initial Commit

---
+++ Faye/Faye.h
@@ -0,0 +1,19 @@
+//
+//  Faye.h
+//  Faye
+//
+//  Created by Alexey Donov on 08/11/2016.
+//  Copyright © 2016 Alexey Donov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Faye.
+FOUNDATION_EXPORT double FayeVersionNumber;
+
+//! Project version string for Faye.
+FOUNDATION_EXPORT const unsigned char FayeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Faye/PublicHeader.h>
+
+
